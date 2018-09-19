b0985ef6249c8d55a56100998545162d1f34437d
Ecko <Ecko@nblan.xyz>
2018-09-19 21:05:41 +0200

    cleanup

--- logrotate.py
+++ logrotate.py
@@ -5,18 +5,9 @@ import os
 import csv
 import shutil
 import datetime
-from time import sleep
 
-logfolder = os.getcwd() + '/log'
-stats = '/stats.csv'
-boosterpack = '/boosterpack.csv'
-files = {'stats' : os.getcwd()+'/log/stats.csv', 'boosterpack' : os.getcwd()+'/log/boosterpack.csv'}
 
-def main():
-    rename()
-    recreate()
-
-def placeholder():
+def rotate():
     today = datetime.date.today()
     week = today.isocalendar()[1]
     os.chdir('./log')
@@ -30,16 +21,5 @@ def placeholder():
             with open(entry, 'a') as file:
                 file.write(first_line)
 
-def rename():
-    #get ISO weeknumber. Week starts on monday
-    today = datetime.date.today()
-    week = today.isocalendar()[1]
-    shutil.move('{}/stats.csv'.format(logfolder),'{}/week{}_stats.csv'.format(logfolder, week))
-
-def recreate():
-    #create a new csv file
-    with open('{}/stats.csv'.format(logfolder), 'a') as file:
-        file.write('date,total trades,daily trades,avg per trade,med per trade,mode per trade, highest per trade, most traded set name, most traded set count, second highest name, second highest count\n')
-
 if __name__ == '__main__':
-    placeholder()
+    rotate()
